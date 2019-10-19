05187809856553e4eece2cb3f7aa6656385bb703
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-19 22:10:34 +0100

    Added two new columns

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,5 +9,5 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(default=timezone.now)
