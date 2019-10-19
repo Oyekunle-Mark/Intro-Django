31860c4e9d7a4a2bceba205164077794a18c8187
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-19 22:15:37 +0100

    Added a new table

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from uuid import uuid4
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,3 +12,7 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
     last_modified = models.DateTimeField(default=timezone.now)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
